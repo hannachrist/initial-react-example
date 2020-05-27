d3d6e9e2edc549a54a40ed69b28bbf5fbf29fc5a
Hanna Christ <christh87@googlemail.com>
2020-05-27 08:47:32 +0200

    updated code on PianoKeysSmart

--- src/components/PianoKeysSmart/PianoKeysSmart.js
+++ src/components/PianoKeysSmart/PianoKeysSmart.js
@@ -2,18 +2,46 @@ import React from 'react';
 
 import '../PianoKeys/PianoKeys.css';
 
+
 class PianoKeysSmart extends React.Component {
+
     constructor() {
         super();
-        
-    };
+
+        this.state = {
+            style: ''
+        };
+    }
+
+    // pressedStyle = () => {
+    //     return {
+    //         boxShadow: 'inset 0px -9px 25px -4px rgba(253, 255, 135, 0.932)'
+    //     };
+    // }
+   
+    changeStyle() {
+        this.setState({
+            boxShadow: 'inset 0px -9px 25px -4px rgba(253, 255, 135, 0.932)'
+        })
+    }
 
     render() {
         return (
             <div className='pianocontainer'>
                 <div className='pianokeys'>
-                    <div className={ 'pianokeys white ' }></div>
-                    <div className={ 'pianokeys black one ' }></div>
+                    <div
+                        className='pianokeys white' 
+                        onKeyDown={(event) => {
+                            if (event.key === 'z') {
+                            this.changeStyle();
+                            }
+                        }}
+                    >
+                    </div>
+                    <div 
+                        className='pianokeys black one'
+                    >
+                    </div>
                     <div className={ 'pianokeys white ' }></div>
                     <div className={ 'pianokeys black two ' }></div>
                     <div className={ 'pianokeys white ' }></div>
